37eb46d3b3ffb0b827a59cd6eb36df3dfcd346b4
vlad <mindlessmuse.666@gmail.com>
2026-06-07 16:48:28 +0300

    feat(db/repo): реализовать TaskStore и методы GetAll, GetByID
    - создана структура TaskStore;
    - реализован метод GetAll для получения всех задач (сортировка по `created_at`);
    - добавлен метод GetByID для получения задачи по `id` с обработкой ошибок.

---
+++ internal/database/tasks.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/MindlessMuse666/task-todo-api/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type TaskStore struct {
+	db *sqlx.DB
+}
+
+func NewTaskStore(db *sqlx.DB) *TaskStore {
+	return &TaskStore{db: db}
+}
+
+func (s *TaskStore) GetAll() ([]models.Task, error) {
+	tasks := []models.Task{}
+
+	query := `
+SELECT id, title, description, completed, created_at, updated_at
+FROM tasks
+ORDER BY created_at DESC;
+`
+
+	if err := s.db.Select(&tasks, query); err != nil {
+		return nil, fmt.Errorf("Ошибка получения всех задач: %w", err)
+	}
+
+	return tasks, nil
+}
+
+func (s *TaskStore) GetByID(id int) (*models.Task, error) {
+	var task models.Task
+
+	query := `
+SELECT id, title, description, completed, created_at, updated_at
+FROM tasks
+WHERE id = $1;
+`
+
+	if err := s.db.Get(&task, query, id); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("Задача с id=%d не найдена", id)
+		}
+
+		return nil, fmt.Errorf("Ошибка получения задачи с id=%d: %w", id, err)
+	}
+
+	return &task, nil
+}

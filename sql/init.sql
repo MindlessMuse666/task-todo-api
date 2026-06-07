1cf5fc8b008daa94693b784137521670312205d7
vlad <mindlessmuse.666@gmail.com>
2026-06-07 09:31:00 +0300

    db(sql): добавлен скрипт сущности tasks | fix(docker): табы docker-compose

---
+++ sql/init.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO tasks (title, description, completed)
+VALUES (
+        'Пошкодничать',
+        'Подёргать микукшу за косички',
+        TRUE
+    ),
+    (
+        'Написать REST API',
+        'Самому написать, ага',
+        FALSE
+    ),
+    (
+        'Релиз аппки',
+        'Деплойку сделать',
+        FALSE
+    );

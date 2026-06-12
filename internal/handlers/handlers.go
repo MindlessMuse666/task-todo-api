94175987d3cbe7752efb3380d155fc8383c4e9ef
vlad <mindlessmuse.666@gmail.com>
2026-06-12 12:48:52 +0300

    feat(handler): реализовать метод GetTask
    реализован метод GetTask для получения всех задачи по ее id.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/MindlessMuse666/task-todo-api/internal/database"
 )
@@ -21,9 +23,31 @@ func (h *Handlers) GetAllTasks(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Ошибка получения задач")
+		return
 	}
 
 	respondWithJSON(w, http.StatusOK, tasks)
+}
+
+func (h *Handlers) GetTask(w http.ResponseWriter, r *http.Request) {
+	pathParts := strings.Split(strings.TrimPrefix(r.URL.Path, "/tasks/"), "/")
+
+	idStr := pathParts[0]
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Некорректный ID задачи")
+		return
+	}
+
+	task, err := h.store.GetByID(id)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, task)
 }
 
 func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {

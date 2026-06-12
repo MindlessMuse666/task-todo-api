ba85a62e5409354ec1025e14f41a488a73c7f0ed
vlad <mindlessmuse.666@gmail.com>
2026-06-12 10:22:54 +0300

    feat(db/repo): реализовано метод Create в TaskStore
    реализован метод Create для создание задачи.

--- internal/database/tasks.go
+++ internal/database/tasks.go
@@ -1,10 +1,11 @@
-// TODO: задокументировать модуль и методы
+// TODO(MindlessMuse): задокументировать модуль и методы
 package database
 
 import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/MindlessMuse666/task-todo-api/internal/models"
 	"github.com/jmoiron/sqlx"
@@ -21,7 +22,7 @@ func NewTaskStore(db *sqlx.DB) *TaskStore {
 func (s *TaskStore) GetAll() ([]models.Task, error) {
 	tasks := []models.Task{}
 
-	// TODO: вынести в отдельный скрипт
+	// TODO(MindlessMuse): вынести в отдельный скрипт
 	query := `
 SELECT id, title, description, completed, created_at, updated_at
 FROM tasks
@@ -38,7 +39,7 @@ ORDER BY created_at DESC;
 func (s *TaskStore) GetByID(id int) (*models.Task, error) {
 	var task models.Task
 
-	// TODO: вынести в отдельный скрипт
+	// TODO(MindlessMuse): вынести в отдельный скрипт
 	query := `
 SELECT id, title, description, completed, created_at, updated_at
 FROM tasks
@@ -51,6 +52,25 @@ WHERE id = $1;
 		}
 
 		return nil, fmt.Errorf("Ошибка получения задачи с id=%d: %w", id, err)
+	}
+
+	return &task, nil
+}
+
+// Create создает новую задачу в базе данных и возвращает ее.
+func (s *TaskStore) Create(input models.CreateTaskInput) (*models.Task, error) {
+	var task models.Task
+
+	// TODO(MindlessMuse): вынести в отдельный скрипт
+	query := `
+INSERT INTO tasks(title, description, completed, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5)
+returning id, title, description, completed, created_at, updated_at;`
+
+	now := time.Now()
+
+	if err := s.db.QueryRowx(query, input.Title, input.Description, input.Completed, now, now).StructScan(&task); err != nil {
+		return nil, fmt.Errorf("Ошибка создания задачи: %w", err)
 	}
 
 	return &task, nil
